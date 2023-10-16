58ab171c771732eab6878dcb929c38be04870987
dariofusco <fdario@hotmail.it>
2023-10-16 14:33:50 +0200

    inserisce chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script>
-export default {
 
-}
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data.results;
+      });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+
 </script>
 
-<template>
+<template></template>
 
-</template>
-
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
