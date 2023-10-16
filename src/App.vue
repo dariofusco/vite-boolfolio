9330a6862661e4b59877ff5b720429fb427c1f10
dariofusco <fdario@hotmail.it>
2023-10-16 17:32:24 +0200

    fix visualizzazione data

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,28 @@ export default {
     };
   },
   methods: {
+
     fetchData() {
       axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
         this.projects = response.data.results.data;
       });
     },
+
     getImageUrl(project) {
       return `http://127.0.0.1:8000/storage/${project.image}`;
     },
+
+    getDateFormat(dateString) {
+      const date = new Date(dateString);
+      return new Intl.DateTimeFormat('default', { dateStyle: 'long' }).format(date);
+    }
+
   },
+
   mounted() {
     this.fetchData();
   },
+
 };
 
 </script>
@@ -34,8 +44,9 @@ export default {
     <div class="row">
       <h1 class="py-3">I Miei Progetti</h1>
       <div class="col-3" v-for="project in projects">
-        <ProjectCard :image="getImageUrl(project)" :name="project.name" :type="project.type.name" :technology="project.technologies"
-          :description="project.description" :date="project.date" :github_link="project.github_link">
+        <ProjectCard :image="getImageUrl(project)" :name="project.name" :type="project.type.name"
+          :technologies="project.technologies" :description="project.description" :date="getDateFormat(project.date)"
+          :github_link="project.github_link">
         </ProjectCard>
       </div>
     </div>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,7 +4,7 @@ export default {
         image: String,
         name: String,
         type: String, 
-        technology: String,
+        technologies: Array,
         description: String,
         date: String,
         github_link: String
@@ -18,7 +18,9 @@ export default {
         <img :src="image" class="card-img-top" alt="">
         <div class="card-body">
             <h5 class="card-title">{{name}}</h5>
-            <span class="badge text-bg-secondary mx-2">{{type}}<span class="badge text-bg-info">{{technology}}</span></span>
+            <span class="badge text-bg-secondary mx-2">{{type}}
+                <span class="badge text-bg-info" v-for="technology in technologies">{{technology.name}}</span>
+            </span>
             <p class="card-text">{{description}}</p>
             <p class="card-subtitle mb-2 text-body-secondary">{{date}}</p>
             <a class="card-link" :href="`${github_link}`">GitHub Link</a>
